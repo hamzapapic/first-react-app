9c50dd697882e88bc6882aa4d71637778420bbcc
hamzapapic <hamza2305@gmail.com>
2023-04-02 15:30:51 +0200

    getting to know react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Greeting from "./components/Greetings/Greeting";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <h1 style={{ color: "#ffffff", fontFamily: "Arial" }}>Logo</h1>
       </header>
+      <Greeting appName="Our First App" username={"Bakir Ujkanovic"}></Greeting>
     </div>
   );
 }
